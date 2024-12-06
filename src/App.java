0c65e20901a3cb73ed824a0d4891190880df90ba
Nesiclor <155731613+Nesiclor@users.noreply.github.com>
2024-12-06 09:21:04 +0100

    Update App.java
    fucked ur mom lmaoooo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Window {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Solitaire - Gecko");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 600);
+
+		CustomPanel panel = new CustomPanel();
+		frame.add(panel);
+
+		frame.setVisible(true);
+	}
 }
